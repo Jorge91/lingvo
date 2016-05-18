becf25b32f4531f5212cefe507df229e9cf7bbcc
Jorgelou <sirjorgete@gmail.com>
2016-05-18 19:49:18 +0200

    installed postgis

--- lingvo/lingvo/settings.py
+++ lingvo/lingvo/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
     'profile',
-    'language'
+    'language',
+    'meeting'
 ]
 
 MIDDLEWARE_CLASSES = [
