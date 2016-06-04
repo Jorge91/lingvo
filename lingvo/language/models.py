c0656dce4368bb2fc3947bb730b4ff95f65fa3ca
Jorgelou <sirjorgete@gmail.com>
2016-06-04 16:42:24 +0200

    name added to language model

--- lingvo/language/models.py
+++ lingvo/language/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Language(models.Model):
     code = models.CharField(unique=True, max_length=10)
-    flag = models.ImageField(upload_to='/flags')
+    name = models.CharField(max_length=30, null=True, blank=True)
+    flag = models.ImageField(upload_to='/flags', blank=True, null=True)
 
     def __unicode__(self):
         return unicode(self.code)
@@ -14,6 +15,9 @@ class User_speaks_language(models.Model):
     user = models.ForeignKey(User, related_name='speaks_user')
     language = models.ForeignKey(Language, related_name='speaks_language')
 
+    class Meta:
+        unique_together = ('user', 'language')
+
     def __unicode__(self):
         return unicode(self.user.username) + ' - ' + unicode(self.language.code)
 
@@ -21,6 +25,9 @@ class User_speaks_language(models.Model):
 class User_practices_language(models.Model):
     user = models.ForeignKey(User, related_name='practices_user')
     language = models.ForeignKey(Language, related_name='practices_language')
+
+    class Meta:
+        unique_together = ('user', 'language')
 
     def __unicode__(self):
         return unicode(self.user.username) + ' - ' + unicode(self.language.code)
