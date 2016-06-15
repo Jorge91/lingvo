bbf112934a61382796f386ef8a64194f8478aaa4
Jorgelou <sirjorgete@gmail.com>
2016-06-15 19:37:58 +0200

    bug endpoint

--- lingvo/language/permissions.py
+++ lingvo/language/permissions.py
@@ -7,7 +7,7 @@ class UserPracticesLanguagesPermission(BasePermission):
     def has_permission(self, request, view):
         if super(UserPracticesLanguagesPermission, self).has_permission(request, view):
             if view.action == 'create':
-                return request.data.get('user') == request.user.id
+                return str(request.data.get('user')) == str(request.user.id)
             if view.action == 'destroy':
                 return request.user.id == User_practices_language.objects.get(pk=view.kwargs.get('pk')).user.id
         return False
@@ -17,7 +17,7 @@ class UserSpeaksLanguagesPermission(BasePermission):
     def has_permission(self, request, view):
         if super(UserSpeaksLanguagesPermission, self).has_permission(request, view):
             if view.action == 'create':
-                return request.data.get('user') == request.user.id
+                return str(request.data.get('user')) == str(request.user.id)
             if view.action == 'destroy':
-                return request.user.id == User_speaks_language.objects.get(pk=view.kwargs.get('pk')).user.id
+                return str(request.user.id) == str(User_speaks_language.objects.get(pk=view.kwargs.get('pk')).user.id)
         return False
--- lingvo/static/js/controllers/meeting/MeetingListController.js
+++ lingvo/static/js/controllers/meeting/MeetingListController.js
@@ -1,11 +1,11 @@
 (function () {
     angular.module('lingvo').controller('MeetingListCtrl', function (NgMap, $scope, $http, $location) {
 
-        var meeting_url = 'api/1.0/meetings/';
+        var meeting_url = 'api/1.0/attendances/';
         $scope.meetings = null;
 
         $http.get(meeting_url).success(function (data) {
-            $scope.meetings = data.features;
+            $scope.meetings = data;
         }).error(function (data) {
 
         });
